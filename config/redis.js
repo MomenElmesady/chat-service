4adf5024f6dda626baf7106c2e61bbf8228cbe78
MomenAshraf08 <m.ashraf@eqardy.com>
2025-06-14 17:40:19 +0300

    redis

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,14 @@
-const Redis = require("ioredis");
+// const Redis = require("ioredis");
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST,
+//   port: 6379,
+// });
+
+// import { Redis } from '@upstash/redis'
+const {Redis} = require("@upstash/redis")
 const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: 6379,
-});
+  url: 'https://great-minnow-12205.upstash.io',
+  token: 'AS-tAAIjcDFhNDk3ZDAwZGM5NzY0ODAzOGQ4YWExMjEyMjFmZDA1M3AxMA',
+})
 
 module.exports = redis;
